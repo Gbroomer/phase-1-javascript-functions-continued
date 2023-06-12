36ba62757ee3ee873a9aa47be71a80f64ef7c07f
Grant Bruemmer <grantbruemmer@gmail.com>
2023-06-12 15:51:01 -0500

    Complete Functions Continued Lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (activity === undefined) {
+        return "This Saturday, I want to roller-skate!";
+    } else {
+        return `This Saturday, I want to ${activity}!`;
+    }
+}
+function mondayWork(work) {
+    if (work === undefined) {
+        return "This Monday, I will go to the office.";
+    } else {
+        return `This Monday, I will ${work}.`;
+    }
+}
+function wrapAdjective(wrapper) {
+    return function (adjective) {
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    }
+}
